716dd1d51aa4feeebb2a43a5de938d688380f2fe
its-michaelroy <72664547+its-michaelroy@users.noreply.github.com>
2024-07-02 11:40:01 -0300

    Starting SDEV Fundamentals Cert course for MSSA

---
+++ DotNet8/Start/Overview/Comments/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Comments
+{
+    class Program
+    {
+        /// XML Comments are used to help provide documentation
+        /// They start with triple-slashes and have a special syntax
+        static void Main(string[] args)
+        {
+            //Single line-comments (3-4 lines max)
+            /*If more than 4 lines then
+            use multiple line comments*/
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
