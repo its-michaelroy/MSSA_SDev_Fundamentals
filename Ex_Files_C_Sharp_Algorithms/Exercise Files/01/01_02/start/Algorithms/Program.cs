1702d035627d32a7baf705d9fa46cce773a3abc9
its-michaelroy <72664547+its-michaelroy@users.noreply.github.com>
2024-07-11 18:07:39 -0300

    Start Algorithms in C#

---
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/01/01_02/start/Algorithms/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithms
+{
+    class Program
+    {
+
+        static Boolean isUpperCase(string s) {
+            return s.All(char.IsUpper);
+        }
+
+        static Boolean isLowerCase(string s) {
+            return s.All(char.IsLower);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(isUpperCase("hello"));
+            Console.WriteLine(isUpperCase("HELLO"));
+            Console.WriteLine(isLowerCase("hello"));
+            Console.WriteLine(isLowerCase("HELLO"));
+        }
+    }
+}
